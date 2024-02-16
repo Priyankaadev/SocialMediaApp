0db16df510a7027611191fd5e3fc44ef72079ab3
Priyankaadev <priyaaswal79@gmail.com>
2024-02-16 19:04:09 +0530

    set up a new project and used material ui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import  Button from '@mui/material'
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      Welcome
+      <Button variant='contained'>Hello world</Button>
+    </>
+  )
+}
+
+export default App
