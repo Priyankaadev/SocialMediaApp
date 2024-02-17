bfab505576fba86c8356eac36fab1aac941ff183
Priyankaadev <priyaaswal79@gmail.com>
2024-02-17 22:05:22 +0530

    set up api

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -9,39 +9,41 @@ import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
-export default function PostCard( ){
+export default function PostCard({ authorFirstName, image, content }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
+            {authorFirstName.substring(0, 1)}
           </Avatar>
         }
-       
+
         title="Shrimp and Chorizo Paella"
         subheader="September 14, 2016"
       />
-      <CardMedia
-        component="img"
-        height="194"
-        image="https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg"
-        alt="cute"
-      />
+      {(image.length > 0) ?
+       (  <CardMedia
+          component="img"
+          height="194"
+          image={image}
+          alt="cute"
+        /> ): null
+
+      }
+
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          {content}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
-       
-        
-     
+
+
+
       </CardActions>
     </Card>
   )
---
+++ src/components/PostCardList/PostCardList.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import PostCard from "../PostCard";
+import axios from "axios";
+
+let array = [
+
+]
+
+function PostCardList() {
+   const [posts, setPosts] = useState( [])
+    useEffect(() => {
+       //we will download the content from dummyapi.io
+       console.log(import.meta.env.VITE_APP_ID);
+       axios.get( "https://dummyapi.io/data/v1/post",
+        {headers: {'app-id': import.meta.env.VITE_APP_ID}})
+        .then( response =>{
+       const responseObject = response.data;
+       setPosts( [...responseObject.data])
+        })
+    }, []); //it renders or execute the callback for the first time when the comp is rendering 
+    // after that it will reexecute the callback when the dependencies updates
+
+    return (
+                ( posts.length ==0) ? 
+                "loading..."
+                : posts.map((post) => <PostCard
+                content={post.text}
+                image={post.image}
+                key={post.id}
+                authorFirstName={post.owner.firstName}
+            />)
+         )
+}
+
+export default PostCardList;
